acd2a2ac05c2a7470c1403f74d9c44aab54ba33e
Victor Onyebuchi <Victor.Onyebuchi@defra.gov.uk>
2024-07-20 16:13:19 +0100

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaturdayStudies
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public double Height { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+
+
+
+        public List<Person> People()
+        {
+            List<Person> pP = new()
+            {
+                new Person { Age = 16, Height = 5.4, DOB = new DateTime(1996, 3, 23), Name = "IyiniOluwa" },
+                new Person { Age = 12, Height = 5.3, DOB = new DateTime(1994, 2, 20), Name = "Iree" },
+                new Person { Age = 34, Height = 5.8, DOB = new DateTime(1990, 1, 01), Name = "Victor" }
+            };
+            
+            return pP;
+
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SaturdayStudies;
+
+Person person = new Person();
+
+List<Person> people = person.People();
+
+string Name = "IyiOluwa";
+
+Name = Name.Replace("O", "1").Replace("u", "2");
+
+string Name2 = ("Victor, Iyioluwa, Iree, Deacon, Victor2");
+
+Console.WriteLine(Name);
+
+//if(people.Count > 0)
+//{
+//	string allTheNmae = string.Empty;
+//	//Foreach lOOP
+//	foreach (var item in people)
+//	{
+//        allTheNmae += $"Name: {item.Name.Replace("I", "V")}, Age: {item.Age.ToString()}, Height: {item.Height}, DOB: {item.DOB}";
+//		var personArray = allTheNmae.Split(',');
+//		Console.WriteLine($"First Person: {personArray[0]}");
+//    }
+
+//	Console.WriteLine(allTheNmae);
+
+//	//FOR LOOP
+//	for (int i = 0; i < people.Count; i++)
+//	{
+        
+//    }
+
+//	//DO WHILE LOOP
+//	do
+//	{
+		
+//	} while (people.Count > 0);
+//}
+
+Console.WriteLine("pROGRAM ENDED");
